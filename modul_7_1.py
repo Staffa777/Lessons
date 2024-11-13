5c3f8cccb921bf62e81405be7e3203edee22d16c
Staffa777 <novostroyws@gmail.com>
2024-11-13 14:39:41 +0700

    Add files via upload

--- modul_7_1.py
+++ modul_7_1.py
@@ -23,10 +23,10 @@ class Shop:
         # Имена продуктов записываются в existing_product_names. Так как строки в файле имеют формат
         # '<название>, <вес>, <категория>', то берется первая часть [0]. В качестве разделителя используется ', '.
         # Для добавления, если строка line не пустая, цикл for перебирает все строки из списка existing_products
-        existing_products_names = {line.split(',') [0] for line in existing_products if line}
+        existing_product_names = {line.split(', ')[0] for line in existing_products if line}
         with open(self.__file_name , 'a') as file:
             for product in products:
-                if product.name is existing_products_names:
+                if product.name is existing_product_names:
                     # Если такой продукт уже есть, то не добавляет и выводит строку
                     # 'Продукт <название> уже есть в магазине'.
                     print(f'Продукт {product.name} уже есть в магазине')
@@ -34,7 +34,7 @@ class Shop:
                     # Добавляет в файл __file_name каждый продукт из products,
                     # если его ещё нет в файле (по названию)
                     file.write(str(product) + '\n')
-                    existing_products_names.add(product.name)
+                    existing_product_names.add(product.name)
 
 
 s1 = Shop()
