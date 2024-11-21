ba94cf84b7ce0e04a370c23c174c759d9b501ba6
Staffa777 <novostroyws@gmail.com>
2024-11-21 13:13:46 +0700

    Add files via upload

--- modul_8_2.py
+++ modul_8_2.py
@@ -1,14 +1,13 @@
-
 def personal_sum(numbers):
     result = 0
     incorrect_data = 0
     for i in numbers:
         try:
-            result += 1
+            result += i
         except TypeError:
             incorrect_data += 1
-            print(f'Некорректный тип данных для подсчёта суммы - {i}')
-    return result , incorrect_data
+            print(f'Не корректный тип данных для подсчёта суммы - {i}')
+    return result, incorrect_data
 
 def calculate_average(numbers):
     try:
@@ -24,8 +23,7 @@ def calculate_average(numbers):
         except ZeroDivisionError:
             return len_
     except TypeError:
-        print(f'В numbers записан некорректный тип данных')
-
+       print('В numbers записан некорректный тип данных')
 print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
 print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
